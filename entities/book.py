79caf49ea17ec258fd9ede800dfd3068c682c6db
Diamond Savior <devbydiamond24.7@gmail.com>
2026-03-19 13:13:33 -0700

    Create Book class skeleton

---
+++ entities/book.py
@@ -0,0 +1,14 @@
+class Book:
+    def __init__(self, title, author, category, isbn, genre):
+        self.title = title
+        self.author = author
+        self.category = category
+        self.isbn = isbn
+        self.genre = genre
+
+    # def display_info(self):
+    #     print(f"Title: {self.title}")
+    #     print(f"Author: {self.author}")
+    #     print(f"Category: {self.category}")
+    #     print(f"ISBN: {self.isbn}")
+    #     print(f"Genre: {self.genre}")
