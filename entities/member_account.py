76d6bfa0c4ae5130674317b7e98e99e98f9afa3b
Diamond Savior <devbydiamond24.7@gmail.com>
2026-03-19 13:12:21 -0700

    Create MemberAccount class skeleton

---
+++ entities/member_account.py
@@ -0,0 +1,11 @@
+class MemberAccount:
+    def __init__(self, library_card_number, name, contact_info, pin):
+        self.library_card_number = library_card_number
+        self.name = name
+        self.contact_info = contact_info
+        self.pin = pin
+
+    # def display_info(self):
+    #     print(f"Library Card Number: {self.library_card_number}")
+    #     print(f"Name: {self.name}")
+    #     print(f"Contact Info: {self.contact_info}")
